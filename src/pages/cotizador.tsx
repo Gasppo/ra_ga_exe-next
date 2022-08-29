ddc29b56c647b2f388733e6904f4b22d61c6be2c
RamaOnate <ramadelarbolito@gmail.com>
2022-08-29 12:12:59 -0300

    Avances en el cotizador piola

--- src/pages/cotizador.tsx
+++ src/pages/cotizador.tsx
@@ -5,17 +5,67 @@ import Head from "next/head";
 import Footer from "../UI/index/Footer";
 import HeaderBar from "../UI/index/HeaderBar";
 import { useGetRole } from "../utils/useGetRole";
+import { useState, useEffect, useCallback } from 'react';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
+import { ClothesCategory } from "@prisma/client";
+
+
+type PriceCheckerModel = {
+    cliente: string,
+    tipoPrenda: ClothesCategory | '',
+    complejidad: string
+}
+
 const Home: NextPage = () => {
 
     const { data } = useSession()
     const isFetching = false
     const role = useGetRole(data?.user?.email || '')
+    const [price, setPrice] = useState(0)
+    const [step, setStep] = useState(0)
+    const [complejidad, setComplejidad] = useState(0)
 
-    const steps = ['uno', 'dos']
+    const [priceCheckerModel, setPriceCheckerModel] = useState<PriceCheckerModel>({ cliente: '', tipoPrenda: '', complejidad: '' })
+    const [clothesTypes, setClothesTypes] = useState([''])
+
+    const [continueButton, setContinueButton] = useState(true)
+    const [backButton, setBackButton] = useState(true)
+
+    const [numberOfModelCorrectFields, setNumberOfModelCorrectFields] = useState(0)
+
+    const steps = ['Modelo', 'Desarrollo', 'Produccion']
 
     const isStepOptional = (index: number) => index === 1
-    console.log(data)
-    
+
+    const advanceStep = () => {
+        if (step < 2)
+            setStep(step + 1)
+        else
+            alert('Cannot advance anymore !!')
+    }
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPriceCheckerModel(prev => ({
+            ...prev,
+            [event.target.name]: event.target.value
+        }))
+        console.log('Model vale:', priceCheckerModel)
+        console.log('Event vale: ', event)
+    }
+
+    const getClothesTypes = useCallback(async () => {
+        const clothesTypes = await fetch('http://localhost:3000/api/clothes/obtain').then(res => res.json())
+        setClothesTypes(clothesTypes)
+        console.log(clothesTypes)
+    }, [])
+
+
+    useEffect(() => {
+        getClothesTypes()
+    }, []);
+
     return (
         <div className="bg-split-white-black">
             <Head>
@@ -27,16 +77,17 @@ const Home: NextPage = () => {
             <HeaderBar />
             <main>
                 <Slide in={true} timeout={500} direction='up'>
-
                     <div className="container mx-auto flex flex-col min-h-screen p-4 bg-white mt-20 rounded-none md:rounded-3xl shadow-2xl">
+
                         <div>
-                            <h1 className="text-5xl md:text-[4rem] leading-normal font-extrabold text-gray-700">
+                            <h1 className="text-5xl md:text-[4rem] leading-normal font-extrabold text-gray-700 md:ml-7" onClick={advanceStep}>
                                 Cotizador
                             </h1>
                         </div>
+
                         <div className="flex flex-row mt-10 justify-between">
-                            <div className="hidden md:flex md:displ md:border-r-2 md:pr-4 md:ml-4 md:w-1/5">
-                                <Stepper activeStep={0} orientation='vertical'>
+                            <div className="hidden md:flex md:displ md:pr-4 md:ml-10 md:w-4/5 items-center justify-start">
+                                <Stepper activeStep={step} orientation='horizontal' alternativeLabel>
                                     {steps.map((label, index) => {
                                         const stepProps: { completed?: boolean } = {};
                                         const labelProps: {
@@ -55,16 +106,41 @@ const Home: NextPage = () => {
                                     })}
                                 </Stepper>
                             </div>
-                            <div className="ml-4" >
-                                Content
+                            <div className="ml-4 text-5xl md:text-[2rem]" >
+                                ${price}
                             </div>
-                            <div className="w-0"/>
+                            <div className="w-0" />
                         </div>
+
+                        <div className="md:mt-9 flex justify-evenly">
+                            <div className="w-2/12 flex justify-center bg-slate-700 place-content-center">
+                                <img src="https://cdn-icons-png.flaticon.com/128/1012/1012573.png" class="max-w-full h-2/4 " alt="..." />
+                            </div>
+                            <div className="grid w-6/12 md:flex-col justify-items-center">
+                                <div className="w-3/6"><TextField disabled fullWidth id="outlined-disabled" label="Cliente" defaultValue={data?.user?.name} /></div>
+                                <div className="md:mt-7 w-3/6">
+                                    <TextField name='tipoPrenda' id="outlined-select-currency" select fullWidth label="Elija categoria" value={priceCheckerModel.tipoPrenda} onChange={handleChange} helperText="Seleccione categoría de la prenda">
+                                        {clothesTypes.map((option) => (
+                                            <MenuItem key={option.name} value={option.name} >
+                                                {option.name}
+                                            </MenuItem>
+                                        ))}
+                                    </TextField>
+                                </div>
+                                <div className="md:mt-7 w-3/6"><TextField fullWidth type='number' name='complejidad' id="outlined-basic" label="Complejidad" variant="outlined" onChange={handleChange} /></div>
+                            </div>
+                        </div>
+
+                        <div className="flex justify-end w-10/12 space-x-4 md:mt-24">
+                            <div><Button variant="outlined" disabled={backButton}>Atrás</Button></div>
+                            <div><Button variant="outlined" disabled={continueButton}>Continuar</Button></div>
+                        </div>
+
                     </div>
                 </Slide>
             </main>
             <Footer />
-        </div>
+        </div >
     );
 };
 

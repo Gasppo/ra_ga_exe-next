8bed3f4a644a0f2949b3ee70972ca9a58d95e8f6
Gasppo <garciagb24@gmail.com>
2022-09-21 15:16:44 -0300

    feat: redirect al inicio

--- src/pages/ordenes.tsx
+++ src/pages/ordenes.tsx
@@ -37,6 +37,13 @@ export default Home;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
     const session = await getSession({ req: context.req });
-
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
     return { props: { session } };
 };

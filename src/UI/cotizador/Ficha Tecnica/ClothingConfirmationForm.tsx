db05503c87b665d5fabcb56a4ef8883180bf1d77
Gasppo <garciagb24@gmail.com>
2022-09-21 23:16:09 -0300

    fix: import

--- src/UI/cotizador/Ficha Tecnica/ClothingConfirmationForm.tsx
+++ src/UI/cotizador/Ficha Tecnica/ClothingConfirmationForm.tsx
@@ -1,5 +1,5 @@
 import { useFormContext } from "react-hook-form";
-import { FichaTecnicaForm } from "../../Types/cotizadorTypes";
+import { FichaTecnicaForm } from "../../Types/fichaTecnicaTypes";
 
 const ClothingConfirmationForm = () => {
 

e449243be86fa6dcae0b4bb9d83f15975b36ca46
Gasppo <garciagb24@gmail.com>
2022-10-20 15:12:05 -0300

    fix: layout tabla usuarios

--- src/UI/index/Dashboard/UsuariosDashboard.tsx
+++ src/UI/index/Dashboard/UsuariosDashboard.tsx
@@ -95,18 +95,8 @@ const UsuariosDashboard = () => {
             <PageTitle title='Usuarios' hasBack />
             <div className="md:mt-9 flex justify-center md:justify-evenly md:mx-10 lg:mx-0">
                 <div className="hidden md:flex flex-col p-4 md:w-full lg:w-2/3 xl:w-3/4 shadow-2xl rounded-3xl bg-gray-100 mx-10">
-                    <div className="text-xl my-8 flex flex-row justify-between" >
-                        <div className='border-gray-400 border-2 rounded-xl w-2/3 p-2 flex items-center shadow-md'>
-                            <SearchIcon className='w-1/12' />
-                            <div className='ml-2 w-8/12'>
-                                <InputBase placeholder="Busqueda por # orden, prenda..." className='w-full' />
-                            </div>
-                        </div>
-                        <div className='flex justify-end items-center w-1/12 mr-1'>
-                            <DownloadIcon fontSize='large' />
-                        </div>
-                    </div>
-                    <div>
+                    
+                    <div className="p-4" >
                         <LoadingIndicator show={isFetchingUsers} >
                             <div style={{ height: 510, width: '100%' }}>
                                 <DataGrid

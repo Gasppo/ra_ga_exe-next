574a3a811d53d0f0c235dd2bd1defbe87f683c44
Gasppo <garciagb24@gmail.com>
2022-10-20 14:21:05 -0300

    nombre de usuario en tabla

--- src/UI/index/Dashboard/OrdenesDashboard.tsx
+++ src/UI/index/Dashboard/OrdenesDashboard.tsx
@@ -34,9 +34,10 @@ const UsuariosDashboard = () => {
         onError: () => addError('Error al traer ordenes')
     })
 
-    const columns: GridColumns = useMemo(() => ([
+    const columns = useMemo((): GridColumns<ExtendedOrdenData> => ([
         { field: 'nombre', headerName: 'Nombre', minWidth: 150, flex: 1 },
-        { field: 'cantidad', headerName: 'Cantidad', minWidth: 75, flex: 1 },
+        { field: '', headerName: 'Cliente', minWidth: 150, flex: 1, valueGetter: (params) => params.row.user.name},
+        { field: 'cantidad', headerName: 'Cantidad', flex: 1 },
         { field: 'estado', headerName: 'Estado', minWidth: 150, valueGetter: (params) => params.row.estado.nombre, flex: 1 },
         { field: 'createdAt', type: 'date', headerName: 'Creación', minWidth: 150, valueFormatter: (params) => new Date(params.value as string).toLocaleDateString(), flex: 1 },
         { field: ' ', headerName: 'Enlace', renderCell: (params) => <Link href={`/orden/${params.row.id}`}><LaunchIcon /></Link>, filterable: false, sortable: false, align: 'center', minWidth: 75, flex: 1 }

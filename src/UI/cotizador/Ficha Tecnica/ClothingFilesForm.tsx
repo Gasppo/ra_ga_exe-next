b88fb0f39153d28ae254a8a17d7314d6b68cea65
Gasppo <garciagb24@gmail.com>
2022-11-02 16:38:34 -0300

    feat: mas formatos de archivo y informacion

--- src/UI/cotizador/Ficha Tecnica/ClothingFilesForm.tsx
+++ src/UI/cotizador/Ficha Tecnica/ClothingFilesForm.tsx
@@ -1,26 +1,9 @@
-import { OrderCreationData } from '@backend/schemas/OrderCreationSchema';
 import FormItem from '@UI/Forms/FormItem';
-import { useEffect } from 'react';
-import { useFormContext } from 'react-hook-form';
 import { clothingFilesLayout } from './forms/clothingFiles.layout';
 
 const ClothingFilesForm = () => {
 
-    const { watch, setValue } = useFormContext<OrderCreationData>()
-
-    const cotizadorData = watch()
-    const files = watch('files')
-    const molderiaBaseDisabled = !cotizadorData.molderiaBase.selected
-    const geometralDisabled = !cotizadorData.geometral.selected
-
-    useEffect(() => {
-        if (molderiaBaseDisabled) setValue('files', files.filter(el => el.section !== 'molderiaBase.files'))
-    }, [molderiaBaseDisabled, setValue, files])
-
-    useEffect(() => {
-        if (geometralDisabled) setValue('files', files.filter(el => el.section !== 'geometral.files'))
-    }, [geometralDisabled, setValue, files])
-
+    
     return (
         <div className="flex md:w-6/12 flex-col justify-center items-baseline mt-10 md:mt-0">
             <div className="form-input-section">

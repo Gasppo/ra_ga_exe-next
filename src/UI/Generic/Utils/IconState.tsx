3a88cf078807984ef33cfae93fc7c3b92d0b0454
Gasppo <garciagb24@gmail.com>
2022-11-03 12:04:57 -0300

    feat: tooltip

--- src/UI/Generic/Utils/IconState.tsx
+++ src/UI/Generic/Utils/IconState.tsx
@@ -1,4 +1,4 @@
-import { Fade, Paper, Popper, PopperPlacementType, Typography } from "@mui/material";
+import { Fade, Paper, Popper, PopperPlacementType, Slide, Tooltip, Typography, Zoom } from "@mui/material";
 import Image from "next/image";
 import React from "react";
 
@@ -30,40 +30,32 @@ const IconBorders = (estado: string) => {
 }
 
 const IconState = (props: IconStateProps) => {
-
-    const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
     const [open, setOpen] = React.useState(false);
-    const [placement, setPlacement] = React.useState<PopperPlacementType>();
 
-    const handleClick =
-        (newPlacement: PopperPlacementType) =>
-            (event: React.MouseEvent<HTMLButtonElement>) => {
-                setAnchorEl(event.currentTarget);
-                setOpen((prev) => placement !== newPlacement || !prev);
-                setPlacement(newPlacement);
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-                setTimeout(() => {
-                    setOpen(false);
-                }, 3000);
-            };
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
 
 
     return (
-        <>
-            <Popper open={open} anchorEl={anchorEl} placement={placement} transition keepMounted={false} >
-                {({ TransitionProps }) => (
-                    <Fade {...TransitionProps} timeout={550}>
-                        <Paper className="rounded-full bg-slate-200">
-                            <Typography sx={{ p: 2 }}>{props.alt}: {props.state}</Typography>
-                        </Paper>
-                    </Fade>
-                )}
-            </Popper>
-
-            <div className={`flex items-center justify-center border-b-4 m-1 h-full  ${IconBorders(props.state)} `} onClick={() => handleClick("bottom")}>
-                <Image src={props.icon} width={30} height={30} alt={props.alt} />
-            </div>
-        </>
+        <div className="group" >
+            <Tooltip open={open} onClose={handleClose} onOpen={handleOpen} title={`${props.alt}: ${props.state}`} TransitionComponent={Zoom} componentsProps={{
+                tooltip: {
+                    className: 'bg-gray-700 text-white text-xs font-bold'
+                }
+            }}>
+                <div className={`flex items-center justify-center border-b-4 m-1 h-full  ${IconBorders(props.state)} cursor-pointer`} >
+                    <div>
+                        <Image src={props.icon} width={30} height={30} alt={props.alt} />
+                    </div>
+                </div>
+            </Tooltip>
+        </div>
     )
 }
 

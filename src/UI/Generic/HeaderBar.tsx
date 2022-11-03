0e4e69580cbc9873892787be320548baf4b423fc
Gasppo <garciagb24@gmail.com>
2022-11-03 14:12:49 -0300

    refactor: unused vars

--- src/UI/Generic/HeaderBar.tsx
+++ src/UI/Generic/HeaderBar.tsx
@@ -1,4 +1,4 @@
-import { signIn, signOut, useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';

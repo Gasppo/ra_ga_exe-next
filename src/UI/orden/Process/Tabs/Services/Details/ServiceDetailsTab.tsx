6ea48673264e2679475718ecd1555385dac7f2a5
Gasppo <garciagb24@gmail.com>
2022-12-04 12:29:46 -0300

    feat: refactors y fichas tecnicas

---
+++ src/UI/orden/Process/Tabs/Services/Details/ServiceDetailsTab.tsx
@@ -0,0 +1,36 @@
+import { ProcessStateTextColors } from '@UI/orden/SelectableOrderProcessItem'
+import { ExtendedOrdenData } from '@utils/Examples/ExtendedOrdenData'
+import React, { useMemo } from 'react'
+
+type Props = {
+  orderData: ExtendedOrdenData
+  selectedProcess: string
+}
+
+const DetailsListElement = ({ title, value }: { title: string, value: string | number }) => {
+  const textColor = ProcessStateTextColors(value.toString())
+
+  return (
+    <div className='flex flex-row items-center space-x-2'>
+      <div className='text-lg'>- <span className='underline'>{title}:</span> <span className={`${textColor} text-sm`}>{value}</span></div>
+    </div>
+  )
+}
+const ServiceDetailsTab = ({ orderData, selectedProcess }: Props) => {
+
+  const currProcess = useMemo(() => orderData?.procesos.find(el => el.id === selectedProcess), [selectedProcess, orderData?.procesos])
+
+  return (
+    <div className='flex flex-col mt-4'>
+      <div className='text-gray-700 text-xl font-semibold'>{currProcess.proceso}</div>
+      <div className='flex flex-col space-y-2 mt-2'>
+        <DetailsListElement title='Estado' value={currProcess.estado} />
+        <DetailsListElement title='Fecha estimada' value={new Date().toLocaleDateString()} />
+        <DetailsListElement title='Ultima modificacion' value={new Date().toLocaleDateString()} />
+      </div>
+
+    </div>
+  )
+}
+
+export default ServiceDetailsTab

365f837be527cff11874bc879c344e19738bfc0b
Gasppo <garciagb24@gmail.com>
2022-12-05 18:24:36 -0300

    fix: images

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,6 @@ export default defineNextConfig({
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "*" }],
     domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com", "cdn-icons-png.flaticon.com", "preview.redd.it", "i.redd.it", "lh3.google.com", "drive.google.com"]
   }
 });
